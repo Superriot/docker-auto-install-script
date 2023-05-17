087dc731c329e5a5e128da66dcfec4b571a175f5
Stefan Mac <stefan.saibel@superriot.net>
2023-05-17 03:36:05 +0200

    add script

---
+++ install-docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Check if Docker is already installed
+if command -v docker &>/dev/null; then
+    echo "Docker is already installed."
+else
+    # Install Docker dependencies
+    echo "Installing Docker dependencies..."
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+    # Install Docker
+    echo "Installing Docker..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+
+    # Add current user to the docker group
+    sudo usermod -aG docker $USER
+
+    # Start Docker service
+    sudo systemctl start docker
+    sudo systemctl enable docker
+
+    echo "Docker has been installed successfully."
+fi
+
+# Check if Docker Compose is already installed
+if command -v docker-compose &>/dev/null; then
+    echo "Docker Compose is already installed."
+else
+    # Install Docker Compose
+    echo "Installing Docker Compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o 
+/usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
+    echo "Docker Compose has been installed successfully."
+fi
+
